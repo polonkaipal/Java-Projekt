17299ef7622a45c852cf09e9bd329f847e4c9996
Geriesz <60937954+Geriesz@users.noreply.github.com>
2021-04-23 13:57:19 +0200

    Image uploader, Datepicker, location List létrehozása

--- src/main/java/hu/unideb/inf/FXMLController.java
+++ src/main/java/hu/unideb/inf/FXMLController.java
@@ -2,10 +2,13 @@ package hu.unideb.inf;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import javafx.scene.control.MenuItem;
 import java.util.ResourceBundle;
+import java.util.Set;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,20 +20,31 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 
 public class FXMLController implements Initializable {
 
     //Main Scene FXML items
+    private int personCreatedIndex = 0;
     private boolean stageLoadedAddPerson = false;
     private boolean stageLoadedPersonDetail = false;
-    private List<Person> szemelyek = new ArrayList<>();
+    private List<Person> persons = new ArrayList<>();
+
+    @FXML
+    private MenuItem addLocation;
+
+    @FXML
+    private MenuItem deleteLocation;
 
     @FXML
     private MenuItem addPersonMenuItem;
 
     @FXML
     private ListView<String> personNameList;
+
+    @FXML
+    private ListView<String> locationNameList;
 
     @FXML
     private ImageView personImage;
@@ -51,11 +65,6 @@ public class FXMLController implements Initializable {
     private Button detailsViewBtn;
 
     @FXML
-    void addPersonToList(ActionEvent event) {
-
-    }
-
-    @FXML
     void detailsViewBtnClick(ActionEvent event) {
         if (!stageLoadedPersonDetail) {
             try {
@@ -66,6 +75,10 @@ public class FXMLController implements Initializable {
                 Datas.personDetailsController = personDetailsController;
                 Stage stage = new Stage();
                 stage.setScene(new Scene(root1));
+
+                stage.setOnCloseRequest((eventt) -> {
+                    stageLoadedPersonDetail = false;
+                });
                 stage.show();
 
             } catch (IOException e) {
@@ -81,7 +94,7 @@ public class FXMLController implements Initializable {
     private MenuItem deleteBtn;
 
     @FXML
-    void delete(ActionEvent event) {
+    void deletePerson(ActionEvent event) {
         //dateOfBirthOutput.setText(Datas.name);
     }
 
@@ -97,6 +110,11 @@ public class FXMLController implements Initializable {
                 Stage stage = new Stage();
                 stage.setScene(new Scene(root1));
                 stage.setTitle("Person Adder");
+
+                //Person adder close handler
+                stage.setOnCloseRequest((eventt) -> {
+                    stageLoadedAddPerson = false;
+                });
                 stage.show();
             } catch (IOException e) {
                 System.out.println("Can't load");
@@ -108,25 +126,80 @@ public class FXMLController implements Initializable {
     }
 
     @FXML
-    void PersonAdder(String name) { 
+    void PersonAdder(String name, LocalDate date, String favouritePlace, double Longitude, double Latitude, String img) {
+        Set<Location> locations = new LinkedHashSet<>();
+        //locations.add(new Location());
+        //locations.get(0).setLongitude(Longitude);
+        //locations.get(0).setLatitude(Latitude);
+
+        persons.add(new Person());
+        persons.get(personCreatedIndex).setName(name);
+        //persons.get(personCreatedIndex).setBirthOfYear(age);
+        persons.get(personCreatedIndex).setLocations(locations);
+        persons.get(personCreatedIndex).setName(name);
+        System.out.println("a dátum "+date);
+        if (img != null) {
+            Image image = new Image(img);
+            personImage.setImage(image);
+        }
+
         personNameList.getItems().add(name);
+        personCreatedIndex++;
         stageLoadedAddPerson = false;
     }
-    
+
     @FXML
-    void listViewHandleClick(MouseEvent event) {
+    void personListViewHandleClick(MouseEvent event) {
         String chosenPerson = personNameList.getSelectionModel().getSelectedItem();
-        System.out.println("clicked on "+ chosenPerson);
-      
+        System.out.println("clicked on " + chosenPerson);
+    }
+
+    @FXML
+    void locationListViewHandleClick(MouseEvent event) {
+        String chosenPerson = locationNameList.getSelectionModel().getSelectedItem();
+        System.out.println("clicked on " + chosenPerson);
+    }
+
+    @FXML
+    void addLocationClicked(ActionEvent event) {
+        if (!stageLoadedAddPerson) {
+            try {
+                stageLoadedAddPerson = true;
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/PersonAddSceneFXML.fxml"));
+                Parent root1 = fxmlLoader.load();
+                PersonAddSceneFXMLController addPersonController = fxmlLoader.getController();
+                Datas.addPersonControllers = addPersonController;
+
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root1));
+                stage.setTitle("Person Adder");
+
+                //Person adder close handler
+                stage.setOnCloseRequest((eventt) -> {
+                    stageLoadedAddPerson = false;
+                });
+                stage.show();
+            } catch (IOException e) {
+                System.out.println("Can't load");
+            }
+        } else {
+            System.out.println("Az oldal már be van töltve");
+        }
+    }
+
+    @FXML
+    void deleteLocationClicked(ActionEvent event) {
+        System.out.println("deleteLocation clicked");
+
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
         //példa
-        
-        szemelyek.add(new Person());
-        szemelyek.get(0).setName("gyuri");
-        personNameList.getItems().add(szemelyek.get(0).getName());
+        //szemelyek.add(new Person());
+        //szemelyek.get(0).setName("gyuri");
+        //personNameList.getItems().add(szemelyek.get(0).getName());
+        locationNameList.getItems().add("Rome");
     }
 }
--- src/main/java/hu/unideb/inf/PersonAddSceneFXMLController.java
+++ src/main/java/hu/unideb/inf/PersonAddSceneFXMLController.java
@@ -5,9 +5,13 @@
  */
 package hu.unideb.inf;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,19 +19,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javax.swing.JFileChooser;
 
 /**
  *
  * @author Szondi
  */
-public class PersonAddSceneFXMLController implements Initializable{
+public class PersonAddSceneFXMLController implements Initializable {
+
     @FXML
-    private TextField dateOfBirthInput;
-    
-    
+    private DatePicker dateOfBirthInput;
 
     @FXML
     private TextField favoritePlaceInput;
@@ -61,27 +69,57 @@ public class PersonAddSceneFXMLController implements Initializable{
 
     @FXML
     private Button addPersonSavebtn;
-    
+
+    @FXML
+    private Button uploadImagebtn;
+
+    @FXML
+    private ImageView addPersonImage;
+
+    private String fileName;
+
+    @FXML
+    private Label fileSelected;
+
+    @FXML
+    public void uploadImageBtnClicked(ActionEvent actionEvent) throws MalformedURLException {
+        fileSelected.setText("");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files",
+                        "*.bmp", "*.png", "*.jpg", "*.gif")); // limit fileChooser options to image files
+        File selectedFile = fileChooser.showOpenDialog(fileSelected.getScene().getWindow());
+
+        if (selectedFile != null) {
+
+            fileName = selectedFile.toURI().toURL().toString();
+
+            Image image = new Image(fileName);
+            addPersonImage.setImage(image);
+        } else {
+            fileSelected.setText("Image file selection cancelled.");
+        }
+    }
+
     @FXML
     private Label errorText;
+
     @FXML
-    void SaddPersonSavebtn(ActionEvent event) {
+    void addPersonSavebtnPressed(ActionEvent event) {
         errorText.setText("");
-        if (nameInput.getText() == null || nameInput.getText().trim().isEmpty() ||
-            dateOfBirthInput.getText() == null || dateOfBirthInput.getText().trim().isEmpty() ||
-            favoritePlaceInput.getText() == null || favoritePlaceInput.getText().trim().isEmpty() ||
-                longitudeInput.getText() == null || longitudeInput.getText().trim().isEmpty() ||
-                latitudeInput.getText() == null || latitudeInput.getText().trim().isEmpty()) 
-        {
-           errorText.setText("Error: Fill in the obligatory inputs!");
-        }
-        else
-        {  
+        if (nameInput.getText() == null || nameInput.getText().trim().isEmpty()
+                || dateOfBirthInput.getValue() == null
+                || favoritePlaceInput.getText() == null || favoritePlaceInput.getText().trim().isEmpty()
+                || longitudeInput.getText() == null || longitudeInput.getText().trim().isEmpty()
+                || latitudeInput.getText() == null || latitudeInput.getText().trim().isEmpty()) {
+            errorText.setText("Error: Fill in the obligatory inputs!");
+        } else {
             try {
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
                 Parent root1 = (Parent) fxmlLoader.load();
                 FXMLController controller = Datas.fxmlController;
-                controller.PersonAdder(nameInput.getText());
+                controller.PersonAdder(nameInput.getText(), dateOfBirthInput.getValue(), favoritePlaceInput.getText(), Double.parseDouble(longitudeInput.getText()), Double.parseDouble(longitudeInput.getText()), fileName);
                 Stage stage = (Stage) addPersonSavebtn.getScene().getWindow();
                 stage.close();
             } catch (IOException e) {
@@ -94,6 +132,6 @@ public class PersonAddSceneFXMLController implements Initializable{
     public void initialize(URL arg0, ResourceBundle arg1) {
         favoritePlaceInput.setText("Rome");
         favoritePlaceInput.setEditable(false);
-        System.out.println("Lefut");
+
     }
 }
