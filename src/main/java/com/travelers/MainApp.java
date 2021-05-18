b58c5d396e2bfeaaf6b99521ecf70c224599ea52
Veress Judit <veress2seljudit@gmail.com>
2021-05-19 02:39:17 +0300

    shutDown() metódus

---
+++ src/main/java/com/travelers/MainApp.java
@@ -0,0 +1,45 @@
+package com.travelers;
+
+import java.sql.SQLException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.h2.tools.Server;
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        startDatabase();
+        
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = (Parent) fxmlLoader.load();
+        FXMLController fxmlController = fxmlLoader.getController();
+        Datas.fxmlController = fxmlController;
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        
+        stage.setTitle("Travellers");
+        stage.setScene(scene);
+        stage.show();
+        
+        shutDownDatabase();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private static void startDatabase() throws SQLException {
+        new Server().runTool("-tcp", "-web", "-ifNotExists");
+    }
+
+    private void shutDownDatabase() {
+        new Server().shutdown();
+    }
+}
