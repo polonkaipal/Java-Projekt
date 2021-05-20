3102cff6f88ca6c7f153750a8c0f0a5f6d437887
Veress Judit <veress2seljudit@gmail.com>
2021-05-20 13:58:31 +0300

    Update FXMLController.java

--- src/main/java/com/travelers/FXMLController.java
+++ src/main/java/com/travelers/FXMLController.java
@@ -6,8 +6,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
